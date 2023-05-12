7ae8573bc9ac31573515fa8d7532ffcd7fbfb9be
MrFat1 <97615672+MrFat1@users.noreply.github.com>
2023-05-12 18:46:42 +0200

    Upload

---
+++ DataBall/src/main/java/com/databall/DataBall/DataBallApplication.java
@@ -0,0 +1,13 @@
+package com.databall.DataBall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataBallApplication.class, args);
+	}
+
+}
